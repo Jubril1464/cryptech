088632603ad0e6866c39bd7f97635a33a21b849d
Jubril1464 <18-30gd050@students.unilorin.edu.ng>
2023-07-14 13:20:25 +0100

    make it better

--- public/index.js
+++ public/index.js
@@ -6,32 +6,44 @@ const closeBtn = document.querySelector('.btn-close')
 const navbar = document.querySelector('.nav-bar')
 const bar1 = document.querySelector('.bar-1')
 const bar2 = document.querySelector('.bar-2')
-const bar3 = document.querySelector('.bar-3')
+const bar3 = document.querySelector('.bar-3');
+const navbarToggle = document.getElementById("navbarToggle");
+const navbarMenu = document.getElementById("navbarMenu");
+const checkBox = document.querySelector('#navbarCheck');
+const navItems = document.querySelector('.nav-items')
+
 
 const stickyNav = function (entries) {
     const [entry] = entries;
     console.log(entry);
     if (!entry.isIntersecting) {
         nav.classList.add('sticky');
-        logo.src ="/assets/CRYPTECHX BLACK.png"
+        logo.src ="./assets/CRYPTECHX BLACK.png"
         bar1.classList.remove('bg-white')
         bar1.classList.add('bg-black')
         bar2.classList.remove('bg-white')
         bar2.classList.add('bg-black')
         bar3.classList.remove('bg-white')
         bar3.classList.add('bg-black')
+        navbar.classList.add('top-14');
+        navItems.classList.remove('text-white')
+        navItems.classList.add('text-black')
+        
 
     }
     else {
 
         nav.classList.remove('sticky');
-        logo.src ="/assets/CRYPTECHX WHITE.png"
+        logo.src ="./assets/CRYPTECHX WHITE.png"
         bar1.classList.add('bg-white')
         bar1.classList.remove('bg-black')
         bar2.classList.add('bg-white')
         bar2.classList.remove('bg-black')
         bar3.classList.add('bg-white')
         bar3.classList.remove('bg-black')
+        navItems.classList.add('text-white')
+        navItems.classList.remove('text-black')
+
 
     }
   };
@@ -45,15 +57,13 @@ const stickyNav = function (entries) {
   headerObserver.observe(header);
 
 
-  const navbarToggle = document.getElementById("navbarToggle");
-  const navbarMenu = document.getElementById("navbarMenu");
-  
+
   navbarToggle.addEventListener("click", () => {
-    console.log('hello')
-    navbar.classList.remove('opacity-0')
-    navbar.classList.add('opacity-1')
+    console.log('hello');
+    navbar.classList.remove('hide')
+    navbar.classList.add('show');
   });
   
 closeBtn.addEventListener('click', () => {
-    navbar.classList.add('opacity-0')
+    navbar.classList.add('hide')
 })
